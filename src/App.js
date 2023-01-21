e6e4ac01928fd1e7a76730df300e9c775fd731da
luka danelia <104508895+Rennn0@users.noreply.github.com>
2023-01-21 20:04:57 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,7 @@ const PRODUCTS = [
   {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
   {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
   {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"},
-  {category: "Saxelebi", price: "$1", stocked: false, name: "luka"},
-  {category: "Saxelebi", price: "$1", stocked: true, name: "zuka"},
-  {category: "Saxelebi", price: "$1", stocked: true, name: "buka"},
-  {category: "Saxelebi", price: "$1", stocked: true, name: "guka"},
-  {category: "gvarebi", price: "$000", stocked: true, name: "danelia"},
-  {category: "gvarebi", price: "$000", stocked: true, name: "shaurma"},
-  {category: "gvarebi", price: "$2", stocked: false, name: "banqo"},
-  {category: "qalaqebi", price: "$99.99", stocked: true, name: "tbilisi"},
-  {category: "qalaqebi", price: "$99.99", stocked: false, name: "poti"},
-  {category: "qalaqebi", price: "$99.99", stocked: true, name: "xashuri"},
-  {category: "nikotini", price: "$8888888", stocked: false, name: "erti"},
-  {category: "nikotini", price: "$8888888", stocked: false, name: "ori"},
-  {category: "nikotini", price: "$8888888", stocked: false, name: "samia"},
-  {category: "nikotini", price: "$8888888", stocked: false, name: "otxi"},
+
 ];
 
 function ProductCategoryRow({ category }) {
